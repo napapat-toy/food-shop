587d0c7dee102d72681c4473edc5c638ed560908
toytoy08 <toy31327@gmail.com>
2020-08-15 03:00:52 +0700

    Update : import data into shoplist then loop with map

--- src/components/Shoplist/index.js
+++ src/components/Shoplist/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import Store from "../Store"
+import { shopdata } from "../../data"
+export default class ShopList extends Component {
 
-export default class ShopList extends Component{
-    render(){
-        return(
-            <h2>Hello ShopList</h2>
+    state = {
+        shops: shopdata
+    }
+
+    render() {
+        console.log(1);
+        const { shops } = this.state;
+        console.log(shops);
+        return (
+            <div>
+                {shops.map(shop => (
+                    <Store key={shop.id} shop={shop} />
+                ))}
+            </div>
         )
     }
 }
--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
-export default class Store extends Component{
-    render(){
-        return(
+export default class Store extends Component {
+    render() {
+        // console.log(this.props);
+        // console.log(this.props.shop);
+        const { id, name, img, detail, address } = this.props.shop
+        return (
             <h2>Hello Store</h2>
         )
     }
